1565ad2b57ad649a2d5da4bf9995c479fbcd99ba
Abhimanue T J <abhimanuetj12@gmail.com>
2023-05-18 16:53:29 +0530

    game board created

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,6 @@
-const Square = ({ value }) => {
+const Square = ({ value, onClick }) => {
   return (
-    <button type="button" className="Square">
+    <button type="button" className="square" onClick={onClick}>
       {value}
     </button>
   );
